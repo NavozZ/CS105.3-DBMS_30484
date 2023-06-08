1e7ba7843ad533f7473291a311427deb83b4274f
NavozZ <135834866+NavozZ@users.noreply.github.com>
2023-06-08 16:39:28 +0530

    Add files via upload
    ADDED THE FILE

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+
+
+public class Main {
+
+  
+    public static void main(String[] args) {
+
+          System.out.println("Adhishtanaka Thiramithu Kulasooriya");
+          System.out.println("Computer Science UGC");
+          
+    }
+    
+}
